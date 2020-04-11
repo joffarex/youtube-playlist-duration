0e373ca8ccfcf81e33d7617b04d013e6b571723b
Tornike Goshadze <joffarex@gmail.com>
2020-04-11 21:56:41 +0400

    Fix wrong path bug

--- bin/main.js
+++ bin/main.js
@@ -1,10 +1,13 @@
 #! /usr/bin/env node
+const { resolve } = require('path');
 const { exec } = require("child_process");
 
 const apiKey = process.argv[2]; // google api key
 const playlistId = process.argv[3]; // playlist id
 
-exec(`node index.js ${apiKey} ${playlistId}`, (error, stdout, stderr) => {
+const path = resolve(__dirname, '../index.js');
+
+exec(`node ${path} ${apiKey} ${playlistId}`, (error, stdout, stderr) => {
   if (error) {
     console.log(`error: ${error.message}`);
     return;
