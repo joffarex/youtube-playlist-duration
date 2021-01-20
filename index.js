be84049c45d731770584d0839c670082201796c5
Tornike Goshadze <joffarex@gmail.com>
2021-01-20 23:47:07 +0400

    Export playlist processing functionality

--- index.js
+++ index.js
@@ -1,50 +1,10 @@
-const { readFileSync } = require('fs');
-const { resolve } = require('path');
-const { google } = require('googleapis');
-const YoutubePlaylistDuration = require('./src/ypd');
+const { processPlaylistId } = require('./src/playlist');
 
 async function main() {
-  try {
-    const playlistId = process.argv[2]; // playlist id
+  const playlistId = process.argv[2];
 
-    if (!playlistId) {
-      throw new Error('Please enter playlistId');
-    }
-
-    let data;
-
-    try {
-      data = readFileSync(resolve(__dirname, 'keys.json'));
-    } catch (e) {
-      console.error('Please first run \'ypd-init\' command');
-    }
-    
-    const { apiKey } = JSON.parse(data.toString());
-
-    if(!apiKey) {
-      throw new Error('Please enter GOOGLE API KEY with using ypd-init command');
-    }
-
-    const ypd = new YoutubePlaylistDuration(apiKey, google.youtube('v3'));
-
-    try {
-      const duration = await ypd.fetchPlaylist(playlistId);
-      console.log(duration);
-    } catch (e) {
-      for (const err of e.errors) {
-        switch (err.reason) {
-          case 'playlistNotFound':
-            console.error(`${err.reason}: Please enter correct playlist id`);
-            break;
-          default:
-            console.error(`${err.reason}: ${err.message}`);
-        }
-      }
-    }
-
-  } catch (e) {
-    console.error(e);
-  }
+  const duration = processPlaylistId(playlistId);
+  console.log(duration);
 }
 
 main();
---
+++ src/playlist.js
@@ -0,0 +1,48 @@
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
+const { google } = require('googleapis');
+const YoutubePlaylistDuration = require('./ypd');
+
+async function processPlaylistId(playlistId) {
+  try {
+    if (!playlistId) {
+      throw new Error('Please enter playlistId');
+    }
+
+    let data;
+
+    try {
+      data = readFileSync(resolve(__dirname, 'keys.json'));
+    } catch (e) {
+      console.error('Please first run \'ypd-init\' command');
+    }
+
+    const { apiKey } = JSON.parse(data.toString());
+
+    if (!apiKey) {
+      throw new Error('Please enter GOOGLE API KEY with using ypd-init command');
+    }
+
+    const ypd = new YoutubePlaylistDuration(apiKey, google.youtube('v3'));
+
+    try {
+      const duration = await ypd.fetchPlaylist(playlistId);
+      return duration;
+    } catch (e) {
+      for (const err of e.errors) {
+        switch (err.reason) {
+          case 'playlistNotFound':
+            console.error(`${err.reason}: Please enter correct playlist id`);
+            break;
+          default:
+            console.error(`${err.reason}: ${err.message}`);
+        }
+      }
+    }
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+module.exports = { processPlaylistId }
