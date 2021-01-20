c171b1dcd1e90f0c14be6e6f97f359c825a12ebd
Tornike Goshadze <joffarex@gmail.com>
2021-01-21 00:14:34 +0400

    Wrap file function inside async call

--- bin/file.js
+++ bin/file.js
@@ -4,22 +4,26 @@ const { createInterface } = require('readline');
 
 const { processPlaylistId } = require('../src/playlist');
 
-const filePath = process.argv[2];
+async function file() {
+  const filePath = process.argv[2];
 
-try {
-  if (!filePath) {
-    throw new Error('Please enter File path to read playlist IDs from');
+  try {
+    if (!filePath) {
+      throw new Error('Please enter File path to read playlist IDs from');
+    }
+
+    const readInterface = createInterface({
+      input: createReadStream(filePath),
+      crlfDelay: Infinity,
+    });
+
+    for await (const playlistId of readInterface) {
+      const duration = await processPlaylistId(playlistId);
+      console.log(`\nPLAYLIST: ${playlistId}\nDURATION: ${duration}\n`);
+    }
+  } catch (e) {
+    console.error(e);
   }
-
-  const readInterface = createInterface({
-    input: createReadStream(filePath),
-    crlfDelay: Infinity,
-  });
-
-  for await (const playlistId of readInterface) {
-    const duration = await processPlaylistId(playlistId);
-    console.log(`\nPLAYLIST: ${playlistId}\nDURATION: ${duration}\n`);
-  }
-} catch (e) {
-  console.error(e);
 }
+
+file();
