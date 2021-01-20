002bba3e61b6b953f416ab989e8e431b856a55df
Tornike Goshadze <joffarex@gmail.com>
2021-01-21 00:13:02 +0400

    Identify line endings

--- bin/file.js
+++ bin/file.js
@@ -11,12 +11,15 @@ try {
     throw new Error('Please enter File path to read playlist IDs from');
   }
 
-  const readInterface = createInterface({ input: createReadStream(filePath), output: process.stdout, console: false });
-
-  readInterface.on('line', (playlistId) => {
-    const duration = processPlaylistId(playlistId);
-    console.log(`\nPLAYLIST: ${playlistId}\nDURATION: ${duration}`);
+  const readInterface = createInterface({
+    input: createReadStream(filePath),
+    crlfDelay: Infinity,
   });
+
+  for await (const playlistId of readInterface) {
+    const duration = await processPlaylistId(playlistId);
+    console.log(`\nPLAYLIST: ${playlistId}\nDURATION: ${duration}\n`);
+  }
 } catch (e) {
   console.error(e);
 }
